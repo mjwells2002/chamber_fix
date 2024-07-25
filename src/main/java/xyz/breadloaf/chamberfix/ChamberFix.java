f2c19e84eb254272751ddb667033f724bdd9db39
Max Henkel <max@maxhenkel.de>
2024-07-25 10:22:28 +0200

    Make the mod work in single-player

--- src/main/java/xyz/breadloaf/chamberfix/ChamberFix.java
+++ src/main/java/xyz/breadloaf/chamberfix/ChamberFix.java
@@ -2,7 +2,7 @@ package xyz.breadloaf.chamberfix;
 
 import de.maxhenkel.admiral.MinecraftAdmiral;
 import de.maxhenkel.configbuilder.ConfigBuilder;
-import net.fabricmc.api.DedicatedServerModInitializer;
+import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.core.BlockPos;
@@ -13,14 +13,14 @@ import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.UUID;
 
-public class ChamberFix implements DedicatedServerModInitializer {
+public class ChamberFix implements ModInitializer {
     public static Path configPath;
     public static Config CONFIG;
 
-    public static HashMap<BlockPos,HashMap<UUID,Long>> VAULT_RESET_TIMERS = new HashMap<>();
+    public static HashMap<BlockPos, HashMap<UUID, Long>> VAULT_RESET_TIMERS = new HashMap<>();
 
     @Override
-    public void onInitializeServer() {
+    public void onInitialize() {
         configPath = FabricLoader.getInstance().getConfigDir().resolve("ChamberFix");
         CONFIG = ConfigBuilder.builder(Config::new).path(configPath.resolve("chamber_fix.properties")).build();
         CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
