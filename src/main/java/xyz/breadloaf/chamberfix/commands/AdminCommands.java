20a7cb1ac7ef9b17a9a5a87cf580b7e25283b596
Max Henkel <max@maxhenkel.de>
2024-07-25 10:38:01 +0200

    Add command feedback

--- src/main/java/xyz/breadloaf/chamberfix/commands/AdminCommands.java
+++ src/main/java/xyz/breadloaf/chamberfix/commands/AdminCommands.java
@@ -1,16 +1,21 @@
 package xyz.breadloaf.chamberfix.commands;
 
+import com.mojang.brigadier.context.CommandContext;
 import de.maxhenkel.admiral.annotations.Command;
 import de.maxhenkel.admiral.annotations.Name;
 import de.maxhenkel.admiral.annotations.RequiresPermissionLevel;
 import de.maxhenkel.admiral.arguments.Players;
 import de.maxhenkel.admiral.arguments.Time;
+import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.entity.player.Player;
+import net.minecraft.network.chat.Component;
 import xyz.breadloaf.chamberfix.ChamberFix;
 
 import java.util.HashMap;
+import java.util.Objects;
 import java.util.UUID;
+
+import static xyz.breadloaf.chamberfix.ChamberFix.VAULT_RESET_TIMERS;
 
 @RequiresPermissionLevel(4)
 @Command("chamberfix")
@@ -18,19 +23,18 @@ public class AdminCommands {
 
     @RequiresPermissionLevel(4)
     @Command("set_reset_time")
-    public void setResetTime(@Name("reset_time") Time time) {
+    public void setResetTime(CommandContext<CommandSourceStack> context, @Name("reset_time") Time time) {
         ChamberFix.CONFIG.resetTimeTicks.set(Long.valueOf(time.get())).save();
+        context.getSource().sendSuccess(() -> Component.literal("Vault reset time set to %s ticks".formatted(time.get())), false);
     }
 
     @RequiresPermissionLevel(4)
     @Command("reset")
-    public void resetPlayer(@Name("vault_location") BlockPos pos, @Name("targets") Players players) {
-        for (Player player : players) {
-            HashMap<UUID,Long> reset_timer = ChamberFix.VAULT_RESET_TIMERS.get(pos);
-            if (reset_timer != null) {
-                reset_timer.put(player.getUUID(), -1L);
-            }
-        }
+    public int resetPlayer(CommandContext<CommandSourceStack> context, @Name("vault_location") BlockPos pos, @Name("targets") Players players) {
+        HashMap<UUID, Long> reset_timer = VAULT_RESET_TIMERS.computeIfAbsent(pos, p -> new HashMap<>());
+        int playersReset = (int) players.stream().map(player -> reset_timer.remove(player.getUUID())).filter(Objects::nonNull).count();
+        context.getSource().sendSuccess(() -> Component.literal("Cooldown reset for %s player(s)".formatted(playersReset)), false);
+        return playersReset;
     }
 
 }
